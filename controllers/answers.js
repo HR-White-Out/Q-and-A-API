92c3e28ec5464b4e322e17502a932243441d40d6
Jake Gilfix <jakegilfix@Control-Surface.attlocal.net>
2022-04-21 16:13:17 -0700

    feat: implements answers controller

--- controllers/answers.js
+++ controllers/answers.js
@@ -1,14 +1,102 @@
-const { answers } = require('../models');
+const { answers, answerPhotos } = require('../models');
+const ERR = require('./error');
 
 module.exports = {
-  getAnswers: (params, queryParams, req, res) => {
-    // do stuff
+  // ***************** SERVES `GET /qa/questions/:question_id/answers` ENDPOINT:
+  getAnswers: (req, res, next) => {
+    const { question_id: questionID } = req?.params || {};
+    const { page = 1, count = 5 } = req?.query || {};
+
+    if (!questionID) return next(ERR.MISSING_QUESTION_ID);
+
+    const options = {
+      where: { question_id: questionID, reported: false },
+      attributes: [
+        'answer_id', 'body', 'date', 'answerer_name', 'helpfulness',
+      ],
+      include: [
+        {
+          model: answerPhotos,
+          as: 'photos',
+          attributes: ['photo_url'],
+        },
+      ],
+      order: [['helpfulness', 'DESC']],
+      offset: (page - 1) * count,
+      limit: count,
+    };
+
+    answers.findAll(options)
+      .then((dbRes) => {
+        res.json(
+          dbRes
+            .map((el) => el.get())
+            .map((a) => ({ ...a, photos: a.photos.map((x) => x.photo_url) })),
+        );
+      })
+      .catch(() => next(ERR.UNRECOGNIZED_QUESTION_ID));
   },
-  addAnswer: (params, bodyParams, req, res) => {
-    // do stuff
+
+  // **************** SERVES `POST /qa/questions/:question_id/answers` ENDPOINT:
+  addAnswer: (req, res, next) => {
+    const { question_id: questionID } = req?.params || {};
+    const {
+      body, name, email, photos,
+    } = req?.body || {};
+
+    if (!questionID) return next(ERR.MISSING_QUESTION_ID);
+    if (!body) return next(ERR.MISSING_ANSWER_BODY);
+
+    const newAnswer = {
+      question_id: questionID,
+      body,
+      answerer_name: name,
+      answerer_email: email,
+      photos: photos.map((p) => ({ photo_url: p })),
+    };
+
+    const options = {
+      include: [
+        {
+          model: answerPhotos,
+          as: 'photos',
+        },
+      ],
+    };
+
+    answers.create(newAnswer, options)
+      .then(() => res.sendStatus(201))
+      .catch(() => next(ERR.BAD_REQUEST));
   },
-  markHelpful: (params, req, res) => {
-    // do stuff
+
+  // ********************* SERVES `PUT /qa/answers/:answer_id/helpful` ENDPOINT:
+  markHelpful: async (req, res, next) => {
+    const { answer_id: answerID } = req?.params || {};
+
+    if (!answerID) return next(ERR.BAD_REQUEST);
+
+    try {
+      const answer = await answers.findOne({ where: { answer_id: answerID } });
+
+      answer.helpfulness += 1;
+
+      await answer.save();
+
+      return res.sendStatus(204);
+    } catch { return next(ERR.BAD_REQUEST); }
   },
-  report: (params, req, res) => {},
+
+  // ********************** SERVES `PUT /qa/answers/:answer_id/report` ENDPOINT:
+  report: (req, res, next) => {
+    const { answer_id: answerID } = req?.params || {};
+
+    if (!answerID) return next(ERR.BAD_REQUEST);
+
+    answers.update(
+      { reported: true },
+      { where: { answer_id: answerID } },
+    )
+      .then(() => res.sendStatus(204))
+      .catch(() => next(ERR.BAD_REQUEST));
+  },
 };
