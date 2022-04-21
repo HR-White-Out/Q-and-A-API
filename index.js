cfa9b9e594f41dae6f11e21d7c8e6b05434ee00e
Jake Gilfix <jakegilfix@Control-Surface.attlocal.net>
2022-04-20 21:19:57 -0700

    feat: implements handlers for each API route

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const controller = require('./controllers');
+// const db = require('./db');
 
 const app = express();
 
@@ -6,8 +8,21 @@ require('dotenv').config();
 
 const { PORT } = process.env;
 
+app.use(express.json());
+
+/* -----------------------------QUESTIONS ROUTES----------------------------- */
+app.get('/qa/questions', controller.questions.getAll);
+app.post('/qa/questions', controller.questions.addQuestion);
+app.put('/qa/questions/:question_id/helpful', controller.questions.markHelpful);
+app.put('/qa/questions/:question_id/report', controller.questions.report);
+/* ------------------------------ANSWERS ROUTES------------------------------ */
+app.get('/qa/questions/:question_id/answers', controller.answers.getAnswers);
+app.post('/qa/questions/:question_id/answers', controller.answers.addAnswer);
+app.put('/qa/answers/:answer_id/helpful', controller.answers.markHelpful);
+app.put('/qa/answers/:answer_id/report', controller.answers.report);
+
 app.get('/', (req, res) => {
-  res.send('Helo World!');
+  res.send('Hello World!');
 });
 
 app.listen(
