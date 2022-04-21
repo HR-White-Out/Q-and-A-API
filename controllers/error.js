5166fda82d17dfda8022eec4936df7c2417edfe5
Jake Gilfix <jakegilfix@Control-Surface.attlocal.net>
2022-04-21 14:54:51 -0700

    feat: adds container for various errors

---
+++ controllers/error.js
@@ -0,0 +1,10 @@
+const createError = require('http-errors');
+
+module.exports = {
+  MISSING_PRODUCT_ID: createError(400, 'No product_id specified'),
+  UNRECOGNIZED_PRODUCT_ID: createError(404, 'Unrecognized product_id.'),
+  MISSING_QUESTION_BODY: createError(400, 'No body specified.'),
+  BAD_REQUEST: createError(400, 'Bad request.'),
+  MISSING_QUESTION_ID: createError(400, 'No question_id specified'),
+  UNRECOGNIZED_QUESTION_ID: createError(404, 'Unrecognized question_id.'),
+};
