ef4347a8fb2d2481ea32928c93eb9e07f6d9ce8d
Jake Gilfix <jakegilfix@Control-Surface.attlocal.net>
2022-04-20 21:23:16 -0700

    (init)

---
+++ controllers/answers.js
@@ -0,0 +1,14 @@
+const { answers } = require('../models');
+
+module.exports = {
+  getAnswers: (params, queryParams, req, res) => {
+    // do stuff
+  },
+  addAnswer: (params, bodyParams, req, res) => {
+    // do stuff
+  },
+  markHelpful: (params, req, res) => {
+    // do stuff
+  },
+  report: (params, req, res) => {},
+};
