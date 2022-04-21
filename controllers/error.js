ce6ebb663fe9b55984c7c2c6696b6abe8a9d56b0
Jake Gilfix <jakegilfix@Control-Surface.attlocal.net>
2022-04-21 16:12:31 -0700

    chore: adds errors for answers controller

--- controllers/error.js
+++ controllers/error.js
@@ -7,4 +7,5 @@ module.exports = {
   BAD_REQUEST: createError(400, 'Bad request.'),
   MISSING_QUESTION_ID: createError(400, 'No question_id specified'),
   UNRECOGNIZED_QUESTION_ID: createError(404, 'Unrecognized question_id.'),
+  MISSING_ANSWER_BODY: createError(400, 'No body specified.'),
 };
