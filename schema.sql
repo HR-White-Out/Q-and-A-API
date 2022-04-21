9c109e3ad9b14d893c60070971ca671749587a98
Jake Gilfix <jakegilfix@Control-Surface.attlocal.net>
2022-04-21 00:29:30 -0700

    bugfix: sets correct default value for dates

--- schema.sql
+++ schema.sql
@@ -3,6 +3,20 @@
 -- USE questions_and_answers;
 
 -- ---
+-- Table 'answer_photos'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS answer_photos;
+
+CREATE TABLE answer_photos (
+  photo_id  SERIAL NOT NULL,
+  answer_id INTEGER NOT NULL,
+  photo_url VARCHAR(1024) NOT NULL,
+  PRIMARY KEY (photo_id)
+);
+
+-- ---
 -- Table 'answers'
 -- 
 -- ---
@@ -13,7 +27,7 @@ CREATE TABLE answers (
   answer_id       SERIAL NOT NULL,
   question_id     INTEGER NOT NULL,
   body            VARCHAR(1000) NOT NULL,
-  date            VARCHAR(32) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  date            VARCHAR(32) NOT NULL DEFAULT extract(epoch from now()),
   answerer_name   VARCHAR(60) DEFAULT 'Anonymous',
   answerer_email  VARCHAR(60) NOT NULL DEFAULT '',
   helpfulness     INTEGER DEFAULT 0,
@@ -32,7 +46,7 @@ CREATE TABLE questions (
   question_id           SERIAL NOT NULL,
   product_id            INTEGER NOT NULL,
   question_body         VARCHAR(1000) NOT NULL,
-  question_date         VARCHAR(32) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  question_date         VARCHAR(32) NOT NULL DEFAULT extract(epoch from now()),
   asker_name            VARCHAR(60) DEFAULT 'Anonymous',
   asker_email           VARCHAR(60) NOT NULL DEFAULT 'NULL',
   question_helpfulness  INTEGER DEFAULT 0,
@@ -41,25 +55,19 @@ CREATE TABLE questions (
 );
 
 -- ---
--- Table 'answer_photos'
--- 
--- ---
-
-DROP TABLE IF EXISTS answer_photos;
-
-CREATE TABLE answer_photos (
-  photo_id  SERIAL NOT NULL,
-  answer_id INTEGER NOT NULL,
-  photo_url VARCHAR(1024) NOT NULL,
-  PRIMARY KEY (photo_id)
-);
-
--- ---
 -- Foreign Keys 
 -- ---
 
 ALTER TABLE answers ADD FOREIGN KEY (question_id) REFERENCES questions (question_id);
 ALTER TABLE answer_photos ADD FOREIGN KEY (answer_id) REFERENCES answers (answer_id);
+
+-- ---
+-- Starting values for new IDs
+-- ---
+
+ALTER SEQUENCE questions_question_id_seq RESTART WITH 3518964;
+ALTER SEQUENCE answers_answer_id_seq RESTART WITH 6879307;
+ALTER SEQUENCE answer_photos_photo_id_seq RESTART WITH 2063760;
 
 -- ---
 -- Table Properties
