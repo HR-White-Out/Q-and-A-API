de311fff80f46f174bb91d01c675bf63db979d8f
Jake Gilfix <jakegilfix@Control-Surface.attlocal.net>
2022-04-21 14:55:34 -0700

    chore: changes answer_photos alias to just 'photos'

--- models/init-models.js
+++ models/init-models.js
@@ -11,7 +11,7 @@ function initModels(sequelize) {
   const questions = _questions(sequelize, DataTypes);
 
   answer_photos.belongsTo(answers, { as: 'answer', foreignKey: 'answer_id' });
-  answers.hasMany(answer_photos, { as: 'answer_photos', foreignKey: 'answer_id' });
+  answers.hasMany(answer_photos, { as: 'photos', foreignKey: 'answer_id' });
   answers.belongsTo(questions, { as: 'question', foreignKey: 'question_id' });
   questions.hasMany(answers, { as: 'answers', foreignKey: 'question_id' });
 
